7174e661b4dde963dad1144934f4d9fe2e45e7a8
Devi <rmdn321@gmail.com>
2022-07-29 12:21:40 -0700

    worked on the questions list

--- index.js
+++ index.js
@@ -8,14 +8,51 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
   {
     type: 'input',
-    name: 'github',
+    name: 'name',
+    message: 'What is the name of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a short description about your project.',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the instructions to be followed to install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What are the steps to be followed to use your project?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'How can someone contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'How can someone run tests associated with your project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license does your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'IBM Public License Version 1.0', 'Mozilla Public License 2.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'username',
     message: 'What is your GitHub username?',
   },
   {
     type: 'input',
-    name: 'github',
-    message: 'What is your GitHub username?',
+    name: 'email',
+    message: 'What is your GitHub email address?',
   },
+ 
 ];
 
 // TODO: Create a function to write README file
@@ -27,9 +64,9 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer
     .prompt(questions)
-    .then((inquirerResponses) => {
+    .then((userAnswers) => {
     console.log('Generating the README file...');
-    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+    writeToFile('README.md', generateMarkdown({ ...userAnswers }));
     })
     .catch( err => {
       console.log(err);
